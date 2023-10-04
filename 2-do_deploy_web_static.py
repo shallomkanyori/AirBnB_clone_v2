0f30092e4c1f4b3e008c566b7cf6cd261087eecd
Shallom Kanyori <shallomkanyori@gmail.com>
2023-10-04 16:29:09 +0000

    Refactor code to catch Exception

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@
         do_deploy
 """
 import os
-from fabric.api import put, run, settings, env
+from fabric.api import put, run, env
 
 env.hosts = ['52.3.241.19', '54.237.48.59']
 
@@ -24,26 +24,24 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
-    with settings(abort_exception=Exception):
-        try:
-            fext = archive_path[archive_path.rfind('/') + 1:]
-            fname = fext[:fext.rfind('.')]
-            rem_path = f"/data/web_static/releases/{fname}"
+    try:
+        fext = archive_path[archive_path.rfind('/') + 1:]
+        fname = fext[:fext.rfind('.')]
+        rem_path = f"/data/web_static/releases/{fname}"
 
-            put(archive_path, "/tmp")
+        put(archive_path, "/tmp")
 
-            run(f"mkdir -p  {rem_path}")
-            run(f"tar -xzf /tmp/{fext} -C {rem_path}")
+        run(f"mkdir -p  {rem_path}")
+        run(f"tar -xzf /tmp/{fext} -C {rem_path}")
 
-            run(f"rm /tmp/{fext}")
+        run(f"rm /tmp/{fext}")
 
-            run(f"mv {rem_path}/web_static/* {rem_path}")
-            run(f"rm -rf {rem_path}/web_static")
+        run(f"mv {rem_path}/web_static/* {rem_path}")
+        run(f"rm -rf {rem_path}/web_static")
 
-            run("rm -rf /data/web_static/current")
-            run(f"ln -s {rem_path} /data/web_static/current")
+        run("rm -rf /data/web_static/current")
+        run(f"ln -s {rem_path} /data/web_static/current")
 
-        except Exception:
-            return False
-
-    return True
+        return True
+    except Exception:
+        return False
