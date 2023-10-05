5681eed6b0e48dfa7573f002fb2a2baad18211fa
Shallom Kanyori <shallomkanyori@gmail.com>
2023-10-05 07:28:23 +0000

    Add Puppet manifest that sets up web_static server
    Add a Puppet manifest that sets up an Nginx server to serve web_static.

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,75 @@
+# Sets up Nginx web server for deployment of web_static
+
+# Update
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['nginx'],
+}
+
+# Install Nginx
+exec {'nginx':
+  command  => 'sudo apt-get -y install nginx',
+  provider => shell,
+  before   => Exec['folders'],
+}
+
+# Create relevant folders
+exec {'folders1':
+  command  => 'mkdir -p /data/web_static/releases/test/',
+  provider => shell,
+  before   => Exec['folders2'],
+}
+
+exec {'folders2':
+  command  => 'mkdir -p /data/web_static/shared/',
+  provider => shell,
+  before   =>  Exec['fake-html'],
+}
+
+# Add fake HTML file
+$cmd_fhtml = 'cat << EOF > /data/web_static/releases/test/index.html
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+EOF
+'
+exec {'fake-html':
+  command  => $cmd_fhtml,
+  provider => shell,
+  before   => Exec['symlink'],
+}
+
+# Create a symlink
+exec {'symlink':
+  command  => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  provider => shell,
+  before   => Exec['chown_ubuntu'],
+}
+
+# Change onwnership of the /data directory
+exec {'chown_ubuntu':
+  command  => 'chown -hR ubuntu:ubuntu /data',
+  provider => shell,
+  before   =>  Exec['conf'],
+}
+
+$new = '\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n'
+$config = "sed -i '/^\\slocation \\/ {$/i\\ ${new}' /etc/nginx/sites-available/default"
+
+# Update configuration
+exec {'conf':
+  command  => $config,
+  provider => shell,
+  before   => Exec['nginx_restart'],
+}
+
+# Apply configuration changes
+exec {'nginx_restart':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
