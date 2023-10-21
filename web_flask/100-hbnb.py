37bcba12bff0db2c34b89ba993ef9ed932f9a409
Shallom Kanyori <shallomkanyori@gmail.com>
2023-10-21 06:12:00 +0000

    Use correct template

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -16,7 +16,7 @@ def index():
     amenities = storage.all(Amenity).values()
     places = storage.all(Place).values()
 
-    return render_template('10-hbnb_filters.html', states=states,
+    return render_template('100-hbnb.html', states=states,
                            amenities=amenities, places=places)
 
 
